ccc7215012dc15dc1f0084f750c574230b7ef4fe
Nicolas Díaz <nicolasfelipediaz@gmail.com>
2022-07-26 11:41:33 -0500

    Initial

---
+++ Objects/main2.d
@@ -0,0 +1,11 @@
+.\objects\main2.o: main2.cpp
+.\objects\main2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.12.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\STM32F7xx.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.12.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\stm32f746xx.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm7.h
+.\objects\main2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main2.o: C:\Keil_v5\ARM\PACK\Keil\STM32F7xx_DFP\2.12.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\system_stm32f7xx.h
